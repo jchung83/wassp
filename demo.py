a364cbc36889e362dca393c033b6659c564e70c9
Javier Chung <javier@MacBook-Air-de-Javier.local>
2022-02-19 20:56:14 -0500

    se logro capturar el ultimo mensaje enviado, reconocer el comando dado y enviar el mensaje, pero falta parar al primer loop del comando leido

--- demo.py
+++ demo.py
@@ -8,6 +8,8 @@ import time
 import sys
 from config import CHROME_PROFILE_PATH
 import re
+import readline
+from string import *
 
 try:
     if sys.argv[1]:
@@ -60,14 +62,20 @@ for group in groups:
     for i in range (1000000):
         text_box = browser.find_element(By.CLASS_NAME, "y8WcF") # IDENTIFICA CAJA DE TEXTO
         a= text_box.text
-        #print(str(a))
+        #print("######   "+  str(a) +"  #######")
+
+
+        a.strip().split("\n")[-1]
+
+        
+        #print(a)
         
 
         try:
 
-         comando = re.search( r"Nose2", str(a))
-         print(comando)
-         print(comando.group(0)) #imprime solo la palabra
+         comando = re.search( r"Munra", str(a))
+         #print(comando)
+         #print(comando.group(0)) #imprime solo la palabra
         except:
          continue
         
@@ -75,17 +83,19 @@ for group in groups:
           print( "enviar mensaje ##########")
 
           input_xpath = "//body/div[@id='app']/div[1]/div[1]/div[4]/div[1]/footer[1]/div[1]/div[1]/span[2]/div[1]/div[2]/div[1]/div[1]"
-          input_box = browser.find_element_by_xpath(input_xpath) 
+          input_box = browser.find_element(By.XPATH, input_xpath) 
           pyperclip.copy(msg)
           input_box.send_keys(Keys.COMMAND +"v" + Keys.RETURN)   # Keys.CONTROL + "v"
           input_box.send_keys(Keys.ENTER)
+
+          continue
 
 
 
 
         time.sleep(10)
 
-        print( "esta chat es el numero  "  + str(i))
+        #print( "esta chat es el numero  "  + str(i))
     '''
 
     #browser.execute_script( "arguments[0].scrollIntoView();", text_box); # SCROLLEA HACIA LA BASE
